a8ca5538a78cbecb158f475771f7d98f2388cd2e
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-01-14 11:50:58 +0100

    Add initial program usage

--- comply/__main__.py
+++ comply/__main__.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+"""
+Make your C follow the rules
+
+Usage:
+  comply -h | --help
+  comply --version
+
+Options:
+  -h --help    Show program help
+  --version    Show program version
+"""
+
+from docopt import docopt
+
+from comply.version import __version__
+
 
 def main():
     """ Entry point for invoking the comply module. """
+
+    arguments = docopt(__doc__, version='comply ' + __version__)
+
+    print(arguments)
 
 
 if __name__ == '__main__':
