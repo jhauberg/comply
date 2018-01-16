7ab73c3c5b30e08b92a889daa98e626822c2d1b6
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-01-16 11:06:39 +0100

    Add rule stub for symbol-used-not-listed
    See #1

--- comply/rules/includes/__init__.py
+++ comply/rules/includes/__init__.py
@@ -1,9 +1,12 @@
 # coding=utf-8
 
 from comply.rules.includes.require_symbols import RequireSymbols
-from comply.rules.includes.unused_symbol import UnusedSymbol
+from comply.rules.includes.symbol_listed_not_used import SymbolListedNotUsed
+from comply.rules.includes.symbol_used_not_listed import SymbolUsedNotListed
+
 
 __all__ = [
     "RequireSymbols",
-    "UnusedSymbol"
+    "SymbolListedNotUsed",
+    "SymbolUsedNotListed"
 ]
