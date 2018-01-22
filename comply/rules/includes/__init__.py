28f2e6b409d7cadcd68a4b02744dba6fd6df6068
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-01-22 20:45:12 +0100

    Add include-guard rule

--- comply/rules/includes/__init__.py
+++ comply/rules/includes/__init__.py
@@ -3,10 +3,12 @@
 from comply.rules.includes.list_symbols import ListSymbols
 from comply.rules.includes.symbol_listed_not_used import SymbolListedNotUsed
 from comply.rules.includes.symbol_used_not_listed import SymbolUsedNotListed
+from comply.rules.includes.include_guard import IncludeGuard
 
 
 __all__ = [
     "ListSymbols",
     "SymbolListedNotUsed",
-    "SymbolUsedNotListed"
+    "SymbolUsedNotListed",
+    "IncludeGuard"
 ]
