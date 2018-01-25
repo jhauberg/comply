a89f2fb68401eaea2a8cfb959c80541c1edd51fc
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-01-25 21:19:43 +0100

    Add ellipsize option for truncating both start and end

--- comply/util.py
+++ comply/util.py
@@ -5,9 +5,10 @@ class Ellipsize:
     middle = 0
     start = 1
     end = 2
+    ends = 3
 
 
-def truncated(text: str, length: int = 40, ellipsize: Ellipsize = Ellipsize.middle):
+def truncated(text: str, length: int=40, ellipsize: int=Ellipsize.middle, from_index: int=None):
     """ Return text truncated to exactly fit a given length.
 
         If text is already shorter than the target length, text returns as is.
@@ -33,6 +34,14 @@ def truncated(text: str, length: int = 40, ellipsize: Ellipsize = Ellipsize.midd
     i = length - len(ellipsis_character)
 
     if ellipsize == Ellipsize.start:
-        return ellipsis_character + text[len(text) - i:len(text)]
+        return ellipsis_character + text[len(text) - i:]
     if ellipsize == Ellipsize.end:
         return text[:i] + ellipsis_character
+    if ellipsize == Ellipsize.ends:
+        j = from_index if from_index is not None else int(len(text) / 2)
+        k = int(length / 2 - len(ellipsis_character))
+
+        if j + k <= len(text):
+            return ellipsis_character + text[j - k:j + k] + ellipsis_character
+        else:
+            return ellipsis_character + text[j - k:]
