423ddb3db5dd7345fa93dc54a2e69553bcfa1f39
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-02-02 14:49:17 +0100

    Refactor suppression of similar violations
    This also has the side-effect of removing suppression of identical
    suggestion/solutions. I think it’s worth it, though, as the code was
    increasingly difficult to comprehend.
    
    This puts more of the general workload into the base Reporter, making
    each specialized reporter more focused on just forming the output.
    
    See #8

--- comply/reporter.py
+++ comply/reporter.py
@@ -9,8 +9,24 @@ from comply.util import truncated, Ellipsize
 
 
 class Reporter:
-    def __init__(self, is_verbose: bool=False):
+    def __init__(self, suppress_similar: bool=True, is_verbose: bool=False):
+        self.suppress_similar = suppress_similar
         self.is_verbose = is_verbose
+
+    def group_by_reason(self, violations):
+        """ Return an ordered dict with violations grouped by their reason. """
+
+        grouped = OrderedDict()
+
+        for violation in violations:
+            reason = violation.which.reason(violation)
+
+            if reason not in grouped:
+                grouped[reason] = []
+
+            grouped[reason].append(violation)
+
+        return grouped
 
     def report_before_checking(self, path: str):
         if self.is_verbose:
@@ -24,69 +40,81 @@ class Reporter:
 
             printdiag(diag)
 
+    def report_similar_results(self, results: list):
+        emitted = 0
+
+        for result in results:
+            printout(result)
+
+            emitted += 1
+
+            if self.suppress_similar and emitted >= self.suppress_after:
+                remaining = len(results) - emitted
+
+                if remaining > 0:
+                    # note that this does not require verbosity flag; if a suppression does occur,
+                    # it should always be mentioned
+                    printdiag('(...{0} more suppressed)'
+                              .format(remaining))
+
+                break
+
     def report(self, violations: list, path: str):
         printout('{0}: {1}'.format(path, violations))
+
+    @property
+    def suppress_after(self) -> int:
+        return 2
 
 
 class StandardReporter(Reporter):
     """ Provides violation output (including suggestions) formatted for human readers. """
 
     def report(self, violations: list, path: str):
-        occurences = []
-        solutions = OrderedDict()
+        # group violations by reason so that we can suppress similar ones
+        grouped = self.group_by_reason(violations)
 
-        for violation in violations:
-            location = '{0}:{1}'.format(
-                truncated(path, length=40, options=Ellipsize.options(at=Ellipsize.middle)),
-                violation.where)
+        for reason, violations in grouped.items():
+            results = []
 
-            reason = '[{0}] {1}'.format(violation.which.name, violation.which.reason(violation))
+            for violation in violations:
+                location = '{0} {1}'.format(
+                    truncated(path, length=40, options=Ellipsize.options(at=Ellipsize.middle)),
+                    violation.where)
 
-            if len(violation.what) > 0:
-                occurence = '{0} {1}'.format(location, violation.what)
-            else:
-                occurence = location
+                why = '{0} [{1}]'.format(reason, violation.which.name)
+                solution = violation.which.solution(violation)
 
-            occurences.append((occurence, reason))
+                output = '{0} {1}\n{2}\n{3}'.format(location, why, violation.what, solution)
 
-            solutions[occurence] = violation.which.solution(violation)
+                results.append(output)
 
-        solutions = without_duplicates(solutions)
-
-        for occurence, reason in occurences:
-            output = '{0} -> {1}'.format(occurence, reason)
-
-            printout(output)
-
-            if occurence in solutions:
-                printout('> {0}'.format(solutions[occurence]))
+            self.report_similar_results(results)
 
 
 class ClangReporter(Reporter):
     """ Provides violation output formatted in a Clang-like fashion. """
 
     def report(self, violations: list, path: str):
-        for violation in violations:
-            absolute_path = os.path.abspath(path)
+        # group violations by reason so that we can suppress similar ones
+        grouped = self.group_by_reason(violations)
 
-            line, column = violation.where
+        for reason, violations in grouped.items():
+            results = []
 
-            if column > 0:
-                location = '{0}:{1}:{2}:'.format(absolute_path, line, column)
-            else:
-                location = '{0}:{1}:'.format(absolute_path, line)
+            for violation in violations:
+                absolute_path = os.path.abspath(path)
 
-            reason = '{0} [{1}]'.format(violation.which.reason(violation), violation.which.name)
-            output = '{0} warning: {1}'.format(location, reason)
+                line, column = violation.where
 
-            printout(output)
+                if column > 0:
+                    location = '{0}:{1}:{2}:'.format(absolute_path, line, column)
+                else:
+                    location = '{0}:{1}:'.format(absolute_path, line)
 
+                why = '{0} [{1}]'.format(reason, violation.which.name)
+                output = '{0} warning: {1}'.format(location, why)
 
-def without_duplicates(pairs: OrderedDict) -> dict:
-    unique_pairs = {}
+                results.append(output)
 
-    for key, value in reversed(pairs.items()):
-        if value not in unique_pairs.values():
-            unique_pairs[key] = value
-
-    return unique_pairs
+            self.report_similar_results(results)
