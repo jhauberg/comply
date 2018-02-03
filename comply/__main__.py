ca908d37af7763557fd71c726b4d94d893acc9d4
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-02-03 17:11:31 +0100

    Add --strict program argument
    Initially, this argument only disables suppression of similar
    violations.
    
    Eventually it should also promote warnings to errors; e.g. explicitly
    noted as 'error:' in terms of Clang-like reports, and (possibly) red
    labels for the standard report.
    
    See #4

--- comply/__main__.py
+++ comply/__main__.py
@@ -5,13 +5,14 @@
 Make your C follow the rules
 
 Usage:
-  comply <input>... [--reporter=<name>] [--verbose]
+  comply <input>... [--reporter=<name>] [--verbose] [--strict]
   comply -h | --help
   comply --version
 
 Options:
   -r --reporter=<name>    Specify type of reported output [default: standard]
-  -v, --verbose           Show diagnostic messages
+  -s --strict             Show all violations (similar violations not suppressed)
+  -v --verbose            Show diagnostic messages
   -h --help               Show program help
   --version               Show program version
 """
@@ -164,6 +165,7 @@ def main():
     reporting_mode = arguments['--reporter']
 
     reporter = make_reporter(reporting_mode)
+    reporter.suppress_similar = not arguments['--strict']
     reporter.is_verbose = arguments['--verbose']
 
     time_since_boot = datetime.datetime.now() - time_started_boot
