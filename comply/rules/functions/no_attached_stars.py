a8bb46d42ce92fdc2db7ca2e0ce09a94dc44b969
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-02-20 20:36:34 +0100

    Make [no-attached-stars] not be limited to only functions
    See #29
    
    One issue with this is that it will also trigger on expressions like
    `(x*y)`, because it thinks this is "a pointer y to type x".

--- comply/rules/functions/no_attached_stars.py
+++ comply/rules/functions/no_attached_stars.py
@@ -3,7 +3,6 @@
 import re
 
 from comply.rules import Rule, RuleViolation
-from comply.rules.functions.pattern import FUNC_BOTH_PATTERN
 
 from comply.printing import Colors
 
@@ -30,15 +29,9 @@ class NoAttachedStars(Rule):
     def collect(self, text: str, filename: str, extension: str):
         offenders = []
 
-        pattern = FUNC_BOTH_PATTERN
+        pattern = r'\((?P<params>[^!@#$+%^{};()]*)\)'
 
-        from comply.util.stripping import strip_function_bodies
-
-        # weed out potential false-positives by stripping the bodies of function implementations
-        # outer most functions will remain as a collapsed body
-        text_without_bodies = strip_function_bodies(text)
-
-        for function_match in re.finditer(pattern, text_without_bodies):
+        for function_match in re.finditer(pattern, text):
             function_result = function_match.group()
             function_parameters = function_match.group('params')
 
@@ -48,10 +41,10 @@ class NoAttachedStars(Rule):
                 offending_index = function_match.start('params') + star_match.start()
 
                 offending_line_number, offending_column = RuleViolation.at(offending_index,
-                                                                           text_without_bodies)
+                                                                           text)
 
                 function_line_number, function_column = RuleViolation.at(function_match.start(),
-                                                                         text_without_bodies)
+                                                                         text)
 
                 parameters_index_in_function_result = function_match.start('params') - function_match.start()
 
@@ -60,12 +53,24 @@ class NoAttachedStars(Rule):
 
                 left_or_right = 'right' if star_match.group().startswith('*') else 'left'
 
-                offender = self.violate(at=(offending_line_number, offending_column),
-                                        lines=[(function_line_number, function_result)],
-                                        meta={'leading_space': function_column - 1,
-                                              'left_or_right': left_or_right,
-                                              'range': offending_range})
+                is_probably_dereference = True
 
-                offenders.append(offender)
+                for i in range(star_match.start() - 1, 0, -1):
+                    if function_parameters[i] == ',':
+                        break
+
+                    if function_parameters[i] != ' ':
+                        is_probably_dereference = False
+
+                        break
+
+                if not is_probably_dereference:
+                    offender = self.violate(at=(offending_line_number, offending_column),
+                                            lines=[(function_line_number, function_result)],
+                                            meta={'leading_space': function_column - 1,
+                                                  'left_or_right': left_or_right,
+                                                  'range': offending_range})
+
+                    offenders.append(offender)
 
         return offenders
