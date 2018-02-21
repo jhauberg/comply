e8674bed4f8bff1b6b6e578b88eec04864e97669
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-02-21 13:38:42 +0100

    Fix [no-attached-stars] line indexing

--- comply/rules/functions/no_attached_stars.py
+++ comply/rules/functions/no_attached_stars.py
@@ -14,45 +14,30 @@ class NoAttachedStars(Rule):
                       suggestion='Add spacing to the {left_or_right} of the pointer star.')
 
     def augment(self, violation: RuleViolation):
-        function_linenumber, function_line = violation.lines[0]
+        line_number, line = violation.lines[0]
 
         from_index, to_index = violation.meta['range'] if 'range' in violation.meta else (0, 0)
 
-        augmented_line = (function_line[:from_index] +
-                          Colors.bad + function_line[from_index:to_index] + Colors.clear +
-                          function_line[to_index:])
+        augmented_line = (line[:from_index] +
+                          Colors.bad + line[from_index:to_index] + Colors.clear +
+                          line[to_index:])
 
-        leading_space = violation.meta['leading_space'] if 'leading_space' in violation.meta else 0
-
-        violation.lines[0] = (function_linenumber, (' ' * leading_space) + augmented_line)
+        violation.lines[0] = (line_number, augmented_line)
 
     def collect(self, text: str, filename: str, extension: str):
         offenders = []
 
         pattern = r'\((?P<params>[^!@#$+%^{};()]*)\)'
 
+        lines = text.splitlines()
+
+
         for function_match in re.finditer(pattern, text):
-            function_result = function_match.group()
             function_parameters = function_match.group('params')
 
             star_pattern = r'\*[^\s,*]|[^\s*]\*'
 
             for star_match in re.finditer(star_pattern, function_parameters):
-                offending_index = function_match.start('params') + star_match.start()
-
-                offending_line_number, offending_column = RuleViolation.at(offending_index,
-                                                                           text)
-
-                function_line_number, function_column = RuleViolation.at(function_match.start(),
-                                                                         text)
-
-                parameters_index_in_function_result = function_match.start('params') - function_match.start()
-
-                offending_range = (parameters_index_in_function_result + star_match.start(),
-                                   parameters_index_in_function_result + star_match.end())
-
-                left_or_right = 'right' if star_match.group().startswith('*') else 'left'
-
                 is_probably_dereference = True
 
                 for i in range(star_match.start() - 1, 0, -1):
@@ -65,10 +50,22 @@ class NoAttachedStars(Rule):
                         break
 
                 if not is_probably_dereference:
+                    offending_index = function_match.start('params') + star_match.start()
+
+                    offending_line_number, offending_column = RuleViolation.at(offending_index,
+                                                                               text)
+
+                    line = lines[offending_line_number - 1]
+
+                    length = star_match.end() - star_match.start()
+
+                    offending_range = (offending_column - 1, offending_column - 1 + length)
+
+                    left_or_right = 'right' if star_match.group().startswith('*') else 'left'
+
                     offender = self.violate(at=(offending_line_number, offending_column),
-                                            lines=[(function_line_number, function_result)],
-                                            meta={'leading_space': function_column - 1,
-                                                  'left_or_right': left_or_right,
+                                            lines=[(offending_line_number, line)],
+                                            meta={'left_or_right': left_or_right,
                                                   'range': offending_range})
 
                     offenders.append(offender)
