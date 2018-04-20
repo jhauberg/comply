8f380031f987c9ec35eebee04d1ec6390f8ba4ef
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-04-20 20:14:10 +0200

    Match rule templates with rule classes
    The build script now automatically looks up all rule classes and tries
    to match them up with the provided templates- if anything is off, e.g.
    naming, the script can now let us now.
    
    This opens up possibilities for grabbing stuff directly from the code,
    without having to type it all up. For example, it might be useful to
    have the rule descriptions in the docs along with the current texts.

--- docs/build.py
+++ docs/build.py
@@ -3,13 +3,38 @@
 import os
 import sys
 import datetime
+import inspect
 
-sys.path.append("..")  # required to import __version__
+sys.path.append("..")  # required to import from the 'comply' package
+
+import comply.rules
 
 from comply.version import __version__
+from comply.rules import *
+
+# find all rule subclasses
+rule_types = [rule_class.__name__ for rule_class in Rule.__subclasses__()]
+
+rules = []
+
+# find all sub-modules inside the .rules package (mod[1] is module object- mod[0] is just the name)
+modules = [mod[1] for mod in inspect.getmembers(comply.rules, inspect.ismodule)]
+
+for rule_type in rule_types:
+    # brute-force our way through each module until we find the one with this rule
+    for rule_module in modules:
+        try:
+            rule_attr = getattr(rule_module, rule_type)
+        except AttributeError as e:
+            pass
+        else:
+            # so we can instantiate it and hold on to it for later
+            rules.append(rule_attr())
+
+            break
 
 template_path = 'base/index.html'
-rules_path = 'base/rules'
+rule_template_path = 'base/rules'
 
 output_path = 'index.html'
 
@@ -28,18 +53,36 @@ date = '{:%B %d, %Y}'.format(datetime.date.today())
 template = template.replace('{{ version }}', __version__)
 template = template.replace('{{ date }}', date)
 
-rules = [file for file in os.listdir(rules_path) if not file.startswith('.')]
-rules = sorted(rules)
+rule_templates = [file for file in os.listdir(rule_template_path) if not file.startswith('.')]
+rule_templates = sorted(rule_templates)
 
 num_rules = 0
+num_excepted_rules = 0
 
-for i, rule_filename in enumerate(rules):
-    rule_path = os.path.join(rules_path, rule_filename)
+for i, rule_filename in enumerate(rule_templates):
+    rule_path = os.path.join(rule_template_path, rule_filename)
+    rule_name, _ = os.path.splitext(rule_filename)
+
+    rule_matches = [rule for rule in rules
+                    if rule.name == rule_name]
+
+    if len(rule_matches) == 0:
+        print('Rule \'{rule}\' was not found in the rules package. '
+              'Are you sure it\'s named right?'
+              .format(rule=rule_name))
+
+        num_excepted_rules += 1
+
+        continue
+    else:
+        rule = rule_matches[0]
 
     with open(rule_path) as rule_file:
         rule_content = rule_file.read()
 
-        if i != len(rules) - 1:
+        rule_content = rule_content.replace('{{ rule_name }}', rule.name)
+
+        if i != len(rule_templates) - num_excepted_rules - 1:
             # append template field at end so we can continue adding rule blocks
             rule_content = rule_content + '\n{{ rules }}'
 
