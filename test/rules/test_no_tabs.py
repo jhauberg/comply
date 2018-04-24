d6fa85641dbf8ae13dc1f15dded269431a95fd6b
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-04-24 20:39:34 +0200

    Add no tabs test

---
+++ test/rules/test_no_tabs.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+import sys
+
+sys.path.append("..")
+
+from comply.rules.misc import NoTabs
+from comply.checking import check, check_text
+
+
+RULE = NoTabs()
+
+
+def test_no_tabs():
+    text = 'source with a	tab'
+    result, _ = check_text(text, [RULE])
+
+    assert result.num_severe_violations == 1
+    assert len(result.violations) == 1
+
+    line_number, column = result.violations[0].where
+
+    assert line_number == 1 and column == 14
+
