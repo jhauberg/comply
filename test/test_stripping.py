6a50f1eddccda3b0a69beec6a0d9c89552ba2568
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-05-18 14:34:52 +0200

    Add case for using the keepends parameter

--- test/test_stripping.py
+++ test/test_stripping.py
@@ -19,6 +19,8 @@ def test_blanked():
     assert blanked(text) == ('    \n'
                              '    ')
 
+    assert blanked(text, keepends=False) == '         '
+
 
 def test_strip_literals():
     text = 'char const * str = "abc";'
