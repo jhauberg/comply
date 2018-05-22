448674765f0979eb4607edd4a6cfc43869d1b72b
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-05-22 18:11:05 +0200

    Refactor to use expected violations helper function

--- test/rules/test_no_todo.py
+++ test/rules/test_no_todo.py
@@ -1,29 +1,20 @@
 # coding=utf-8
 
 from comply.rules.standard import NoTodo
-from comply.checking import check_text
+
+from test.rules.expect import match_triggers
 
 
-RULE = NoTodo()
+def test_no_todo():
+    texts = [
+        # triggers
+        'source with a // ↓todo: find me',
+        'source with a // ↓TODO: find me',
+        'source           ↓todo: f',
+        # non-triggers
+        'source with a // todo don\'t find me',
+        'source with a // TODO don\'t find me',
+        'source todo'
+    ]
 
-
-def test_no_todo_triggers():
-    triggers = ['source with a // todo: find me',
-                'source with a // TODO: find me',
-                'source           todo: f']
-
-    for text in triggers:
-        result = check_text(text, [RULE])
-
-        assert len(result.violations) == 1
-        assert result.violations[0].where == (1, 18)
-
-def test_no_todo_non_triggers():
-    triggers = ['source with a // todo don\'t find me',
-                'source with a // TODO don\'t find me',
-                'source todo']
-
-    for text in triggers:
-        result = check_text(text, [RULE])
-
-        assert len(result.violations) == 0
+    match_triggers(texts, NoTodo)
