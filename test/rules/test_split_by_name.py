917d2928b6a3310d572671ae9bcc0c6a4db26c29
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-05-27 17:00:27 +0200

    Add split-by-name test

---
+++ test/rules/test_split_by_name.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+from comply.rules.standard import SplitByName
+
+from test.rules.expect import match_triggers
+
+
+def test_split_by_name():
+    texts = [
+        # triggers
+        'void ↓func(void) { ... }',
+        # non-triggers
+        'void func();',
+        ('void\n'
+         'func(void) { ... }'),
+        # false-positives
+        ('#define ↓YouMessage(pointer, prefix, text) \\\n'
+         '    strcat((YouPrefix(pointer, prefix, text), pointer), text)\n'
+         '\n'
+         'void You\n'
+         'VA_DECL(const char *, line)\n'
+         '{')
+    ]
+
+    match_triggers(texts, SplitByName)
