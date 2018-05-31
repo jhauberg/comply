17b4b4d392bac0fc1a84ece6d0a38a80d62a4dff
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-05-31 13:42:54 +0200

    Refactor and simplify matching

--- comply/rules/standard/no_todo.py
+++ comply/rules/standard/no_todo.py
@@ -13,40 +13,23 @@ class NoTodo(Rule):
     def __init__(self):
         Rule.__init__(self, name='no-todo',
                       description='TODO: {todo}',
-                      suggestion='Consider promoting this issue to a full report in your issue tracker.')
+                      suggestion='Consider promoting this note to a full report in your issue tracker.')
 
-    pattern = re.compile(r'(TODO:|todo:)(.*)')
-
-    def augment(self, violation: RuleViolation):
-        line_number, line = violation.lines[0]
-
-        from_index, to_index = violation.meta['range']
-
-        augmented_line = (line[:from_index] +
-                          Colors.BAD + line[from_index:to_index] + Colors.RESET +
-                          line[to_index:])
-
-        violation.lines[0] = (line_number, augmented_line)
+    pattern = re.compile((r'(?:todo|fixme):'  # match notes in any combination of upper or lowercase
+                          r'(.*)'),           # and everything until end of line
+                         re.IGNORECASE)
 
     def collect(self, file: CheckFile):
         offenders = []
 
         for match in self.pattern.finditer(file.original):
-            line_number, column = file.line_number_at(match.start())
-
-            line = file.lines[line_number - 1]
-
-            message = match.group(2)
+            message = match.group(1)
             message = truncated(message.strip(),
                                 length=60,
                                 options=Ellipsize.options(at=Ellipsize.end))
 
-            column_end = column + len(match.group())
-
-            offender = self.violate(at=(line_number, column),
-                                    lines=[(line_number, line)],
-                                    meta={'range': (column - 1, column_end),
-                                          'todo': message})
+            offender = self.violate_at_match(file, at=match)
+            offender.meta = {'todo': message}
 
             offenders.append(offender)
 
