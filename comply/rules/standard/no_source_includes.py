359793e06c80bd980c24128182c6398ce9e27c9a
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-05-31 13:22:34 +0200

    Don't search original text because it can produce false-positives

--- comply/rules/standard/no_source_includes.py
+++ comply/rules/standard/no_source_includes.py
@@ -10,15 +10,18 @@ class NoSourceIncludes(Rule):
     def __init__(self):
         Rule.__init__(self, name='no-src-includes',
                       description='Don\'t include source files',
-                      suggestion='Remove #include directive.')
+                      suggestion='Find a way to remove the #include directive.')
 
     pattern = re.compile(INCLUDE_PATTERN)
 
     def collect(self, file: CheckFile):
         offenders = []
 
-        for inclusion in self.pattern.finditer(file.original):
-            included_file = inclusion.group('filename').strip()
+        for inclusion in self.pattern.finditer(file.stripped):
+            # note that we can't just grab the string captured by the 'filename' group in this case
+            # because we're searching on stripped source (which might have stripped literals)
+            included_file = file.original[inclusion.start('filename'):inclusion.end('filename')]
+            included_file = included_file.strip()
 
             if included_file.endswith('.c'):
                 offender = self.violate_at_match(file, at=inclusion)
